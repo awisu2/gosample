f7aea7346ca86cc6b4add5917c61bb5dd60028ac
awisu2 <awisu2@gmail.com>
2016-12-17 15:08:20 +0900

    any value

--- interface/main.go
+++ interface/main.go
@@ -22,11 +22,54 @@ func (self Multipl) Calc(a int, b int) int {
 	return a * b
 }
 
+// AnyValue
+type Any interface{}
+
 func main() {
-	sample(Plus{}, 2, 3)    // 5
-	sample(Multipl{}, 2, 3) // 6
+	sameFunc(Plus{}, 2, 3)    // 5
+	sameFunc(Multipl{}, 2, 3) // 6
+
+	anyValue()
 }
 
-func sample(in I1, a int, b int) {
-	fmt.Println("Calc : ", in.Calc(a, b))
+// 関数がそろっていれば処理を呼ぶことが可能
+// ただし、プロパティ値にはアクセスできない
+func sameFunc(in I1, a int, b int) {
+	fmt.Println(in.Calc(a, b))
+}
+
+// どんな値でも受け付ける
+func anyValue() {
+	var i Any = 1
+	var s Any = "s"
+	var b Any = true
+	var p Any = Plus{}
+
+	fmt.Println(i) // 1
+	fmt.Println(s) // s
+	fmt.Println(b) // true
+	fmt.Println(p) // {}
+
+	// 1
+	if v, ok := i.(int); ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("not int")
+	}
+
+	// not int
+	if v, ok := s.(int); ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("not int")
+	}
+
+	// map[b:true p:{} i:1 s:s]
+	m := map[string]Any{
+		"i": i,
+		"s": s,
+		"b": b,
+		"p": p,
+	}
+	fmt.Println(m)
 }
